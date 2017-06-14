corrected updates of moving-average

crnn_train.py
@@ -9,14 +9,11 @@ from config import Conf
 from dataset import Dataset
 from decoding import simpleDecoder, evaluation_metrics
 import argparse
+import os
 
 
 # CONFIG PARAMETERS
 # -----------------
-
-# Limit the usage of GPU memory to 30%
-config_sess = tf.ConfigProto()
-config_sess.gpu_options.per_process_gpu_memory_fraction = 0.3
 
 # config = Conf(n_classes=37,
 #               train_batch_size=128,
@@ -34,10 +31,7 @@ config_sess.gpu_options.per_process_gpu_memory_fraction = 0.3
 #               list_n_hidden=[256, 256],
 #               max_len=24)
 
-session = tf.Session(config=config_sess)
-
-
-def crnn_train(conf, sess=session):
+def crnn_train(conf, sess):
 
     # PLACEHOLDERS
     # ------------
@@ -68,15 +62,22 @@ def crnn_train(conf, sess=session):
     global_step = tf.Variable(0)
     learning_rate = tf.train.exponential_decay(conf.learning_rate, global_step, conf.decay_steps,
                                                conf.decay_rate, staircase=True)
+
     if conf.optimizer == 'ada':
-        optimizer = tf.train.AdadeltaOptimizer(learning_rate).minimize(ctc.loss, global_step=global_step)
+        optimizer = tf.train.AdadeltaOptimizer(learning_rate)
     elif conf.optimizer == 'adam':
-        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(ctc.loss, global_step=global_step)
+        optimizer = tf.train.AdamOptimizer(learning_rate, beta1=0.5)
     elif conf.optimizer == 'rms':
-        optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(ctc.loss, global_step=global_step)
+        optimizer = tf.train.RMSPropOptimizer(learning_rate)
     else:
         print('Error, no optimizer. RMS by default.')
-        optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(ctc.loss, global_step=global_step)
+        optimizer = tf.train.RMSPropOptimizer(learning_rate)
+
+    # To save batch parameters (moving average/variance), see
+    # https://www.tensorflow.org/api_docs/python/tf/contrib/layers/batch_norm
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = optimizer.minimize(ctc.loss, global_step=global_step)
 
     # ERRORS EVALUATION
     # -----------------
@@ -124,6 +125,9 @@ def crnn_train(conf, sess=session):
                         path=conf.dataSet,
                         mode='val')
 
+    images_batch_eval, label_set_eval, seq_len_eval = data_test.nextBatch(conf.evalBatchSize)
+    images_batch_eval = np.expand_dims(images_batch_eval, axis=-1)
+
     # RUN SESSION
     # -----------
 
@@ -137,7 +141,7 @@ def crnn_train(conf, sess=session):
 
         images_batch = np.expand_dims(images_batch, axis=-1)
 
-        cost, _, step = sess.run([ctc.cost, optimizer, global_step],
+        cost, _, step = sess.run([ctc.cost, train_op, global_step],
                                  feed_dict={
                                            x: images_batch,
                                            keep_prob: 0.7,
@@ -150,19 +154,17 @@ def crnn_train(conf, sess=session):
 
         # Eval accuarcy
         if step != 0 and step % conf.evalInterval == 0:
-            images_batch_eval, label_set_eval, seq_len_eval = data_test.nextBatch()
-            images_batch_eval = np.expand_dims(images_batch_eval, axis=-1)
 
             cost_eval, raw_pred = sess.run([ctc.cost, crnn.rawPred],
-                                feed_dict={
-                                            x: images_batch_eval,
-                                            keep_prob: 1.0,
-                                            is_training: False,
-                                            # rnn_seq_len: test_seq_len,
-                                            input_ctc_seq_len: test_seq_len,
-                                            target_seq_len: seq_len_eval,
-                                            labels: label_set_eval[1],
-                                           })
+                                           feed_dict={
+                                                        x: images_batch_eval,
+                                                        keep_prob: 1.0,
+                                                        is_training: False,
+                                                        # rnn_seq_len: test_seq_len,
+                                                        input_ctc_seq_len: test_seq_len,
+                                                        target_seq_len: seq_len_eval,
+                                                        labels: label_set_eval[1],
+                                                       })
 
             str_pred = simpleDecoder(raw_pred)
             # acc = eval_accuracy(str_pred, label_set_eval[0])
@@ -209,12 +211,19 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--decay_steps', type=int, help='Decay steps for learning rate', default=1000)
     parser.add_argument('-e', '--eval_interval', type=float, help='Evaluation interval (steps)', default=500)
     parser.add_argument('-o', '--optimizer', type=str, help='Optimizer (ada, adam or rms)', default='rms')
+    parser.add_argument('-g', '--gpu', type=str, help='GPU device (0, 1 or 10 (both))), default=CPU', default='')
+    parser.add_argument('-mf', '--memory_fraction', type=float, help='Fraction of memory per GPU used', default=0.3)
 
     args = parser.parse_args()
 
+    # Limit the usage of GPU memory to 30%
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    config_sess = tf.ConfigProto()
+    config_sess.gpu_options.per_process_gpu_memory_fraction = args.memory_fraction
+
     config = Conf(n_classes=37,
                   train_batch_size=128,
-                  # eval_batch_size=64,
+                  eval_batch_size=1000,
                   learning_rate=args.learning_rate,
                   decay_rate=args.decay_rate,
                   decay_steps=args.decay_steps,
@@ -223,11 +232,13 @@ if __name__ == '__main__':
                   max_epochs=100,
                   eval_interval=args.eval_interval,
                   save_interval=2500,
-                  file_writer='../{}_d{}-l{}'.format(args.optimizer, args.decay_rate, args.learning_rate),
+                  file_writer='../corrected/{}_d{}-l{}'.format(args.optimizer, args.decay_rate, args.learning_rate),
                   data_set='/home/soliveir/NAS-DHProcessing/mnt/ramdisk/max/90kDICT32px/',
-                  model_dir='../model-crnn-{}_d{}-l{}'.format(args.optimizer, args.decay_rate, args.learning_rate),
+                  model_dir='../corrected/model-crnn-{}_d{}-l{}'.format(args.optimizer, args.decay_rate, args.learning_rate),
                   input_shape=[32, 100],
                   list_n_hidden=[256, 256],
                   max_len=24)
 
-    crnn_train(conf=config)
+    session = tf.Session(config=config_sess)
+
+    crnn_train(conf=config, sess=session)

model.py
@@ -22,7 +22,7 @@ class Model:
 # ----------------------------------------------------------
 
 
-def weightVar(shape, mean=0.0, stddev=0.1, name='weights'):
+def weightVar(shape, mean=0.0, stddev=0.02, name='weights'):
     initW = tf.truncated_normal(shape=shape, mean=mean, stddev=stddev)
     return tf.Variable(initW, name=name)
 