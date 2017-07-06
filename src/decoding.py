8cade604d0525d1cc44c1621133dfcd9463eebed
soliveir <sofia.oliveiraares@epfl.ch>
2017-07-06 17:03:37 +0200

    considering case of batch-size = 1

--- src/decoding.py
+++ src/decoding.py
@@ -9,9 +9,18 @@ def get_words_from_chars(characters_list, sequence_lengths, name='chars_conversi
         def my_func(coords):
             return tf.reduce_join(characters_list[coords[0]:coords[1]])
 
-        end_coords = tf.cumsum(sequence_lengths)
-        start_coords = tf.concat([[0], end_coords[:-1]], axis=0)
-        coords = tf.stack([start_coords, end_coords], axis=1)
-        coords = tf.cast(coords, dtype=tf.int32)
+        def coords_several_sequences():
+            end_coords = tf.cumsum(sequence_lengths)
+            start_coords = tf.concat([[0], end_coords[:-1]], axis=0)
+            coords = tf.stack([start_coords, end_coords], axis=1)
+            coords = tf.cast(coords, dtype=tf.int32)
+            return tf.map_fn(my_func, coords, dtype=tf.string)
 
-        return tf.map_fn(my_func, coords, dtype=tf.string)
+        def coords_single_sequence():
+            return tf.reduce_join(characters_list)
+
+        words = tf.cond(tf.shape(sequence_lengths)[0] > 1,
+                        true_fn=lambda: coords_several_sequences(),
+                        false_fn=lambda: coords_single_sequence())
+
+        return words
