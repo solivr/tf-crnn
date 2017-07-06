d6c87af679e72238927db6024f5af98e95117717
soliveir <sofia.oliveiraares@epfl.ch>
2017-07-06 17:05:40 +0200

    minor changes

--- src/config.py
+++ src/config.py
@@ -3,12 +3,22 @@ __author__ = 'solivr'
 
 
 class Conf:
-    def __init__(self, n_classes=None, train_batch_size=100, learning_rate=0.001, decay_rate=0.96, decay_steps='1000',
+    def __init__(self, n_classes=None,
+                 train_batch_size=100,
+                 learning_rate=0.001,
+                 decay_rate=0.96,
+                 decay_steps='1000',
                  optimizer='rms',
                  eval_batch_size=200,
-                 max_iteration=10000, max_epochs=50, eval_interval=100,
-                 save_interval=1000, file_writer=None, model_dir=None, data_set='../data', max_len=28,
-                 input_shape=[32, 100], list_n_hidden=[256, 256], summary_dir='./graph'):
+                 max_iteration=10000,
+                 max_epochs=50,
+                 eval_interval=100,
+                 save_interval=1000,
+                 model_dir=None,
+                 data_set='../data',
+                 max_len=24,
+                 input_shape=[32, 100],
+                 list_n_hidden=[256, 256]):
         self.nClasses = n_classes
         self.trainBatchSize = train_batch_size
         self.learning_rate = learning_rate
@@ -20,7 +30,6 @@ class Conf:
         self.maxEpochs = max_epochs
         self.evalInterval = eval_interval
         self.saveInterval = save_interval
-        self.fileWriter = file_writer
         self.modelDir = model_dir
         self.dataSet = data_set
         self.maxLength = max_len
@@ -32,4 +41,3 @@ class Conf:
         except IndexError:
             self.imgC = 1
         self.listNHidden = list_n_hidden
-        self.summaryDir = summary_dir
