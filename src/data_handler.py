8e970110f739977197eba2aae34d46c26c003322
soliveir <sofia.oliveiraares@epfl.ch>
2017-10-04 12:07:03 +0200

    general refactoring, addition of multiple alphabets usage, bug fixed and improvements

--- src/data_handler.py
+++ src/data_handler.py
@@ -3,24 +3,21 @@ __author__ = 'solivr'
 
 import tensorflow as tf
 import numpy as np
-import os
-from .config import Params
+from .config import Params, CONST
 
 
-def data_loader(csv_filename, params: Params, cursor=0, batch_size=128, data_augmentation=False,
+def data_loader(csv_filename, params: Params, batch_size=128, data_augmentation=False,
                 num_epochs=None, image_summaries=False):
 
     def input_fn():
         # Choose case one csv file or list of csv files
         if not isinstance(csv_filename, list):
-            # dirname = os.path.dirname(csv_filename)
-            filename_queue = tf.train.string_input_producer([csv_filename], num_epochs=num_epochs)
+            filename_queue = tf.train.string_input_producer([csv_filename], num_epochs=num_epochs, name='filename_queue')
         elif isinstance(csv_filename, list):
-            # dirname = os.path.dirname(csv_filename[0])
-            filename_queue = tf.train.string_input_producer(csv_filename, num_epochs=num_epochs)
+            filename_queue = tf.train.string_input_producer(csv_filename, num_epochs=num_epochs, name='filename_queue')
 
         # Skip lines that have already been processed
-        reader = tf.TextLineReader(name='CSV_Reader', skip_header_lines=cursor)
+        reader = tf.TextLineReader(name='CSV_Reader', skip_header_lines=0)
         key, value = reader.read(filename_queue, name='file_reading_op')
 
         default_line = [['None'], ['None']]
@@ -31,17 +28,47 @@ def data_loader(csv_filename, params: Params, cursor=0, batch_size=128, data_aug
                                          data_augmentation=data_augmentation, padding=True)
 
         to_batch = {'images': image, 'images_widths': img_width, 'filenames': path, 'labels': label}
-        prepared_batch = tf.train.batch(to_batch,
-                                        batch_size=batch_size,
-                                        num_threads=15, capacity=3000,
-                                        dynamic_pad=False)
+        prepared_batch = tf.train.shuffle_batch(to_batch,
+                                                batch_size=batch_size,
+                                                min_after_dequeue=500,
+                                                num_threads=15, capacity=3000,
+                                                allow_smaller_final_batch=False,
+                                                name='prepared_batch_queue')
 
         if image_summaries:
             tf.summary.image('input/image', prepared_batch.get('images'), max_outputs=1)
+        tf.summary.text('input/labels', prepared_batch.get('labels')[:10])
+        tf.summary.text('input/widths', tf.as_string(prepared_batch.get('images_widths')))
 
         return prepared_batch, prepared_batch.get('labels')
 
     return input_fn
+
+
+def image_reading(path, resized_size=None, data_augmentation=False, padding=False):
+    # Read image
+    image_content = tf.read_file(path, name='image_reader')
+    image = tf.cond(tf.equal(tf.string_split([path], '.').values[1], tf.constant('jpg', dtype=tf.string)),
+                    true_fn=lambda: tf.image.decode_jpeg(image_content, channels=1, try_recover_truncated=True),
+                    false_fn=lambda: tf.image.decode_png(image_content, channels=1))
+
+    # Data augmentation
+    if data_augmentation:
+        image = augment_data(image)
+
+    # Padding
+    if padding:
+        with tf.name_scope('padding'):
+            image, img_width = padding_inputs_width(image, resized_size, increment=CONST.DIMENSION_REDUCTION_W_POOLING)
+    # Resize
+    elif resized_size:
+        image = tf.image.resize_images(image, size=resized_size)
+        img_width = tf.shape(image)[1]
+    else:
+        raise NotImplementedError
+
+    with tf.control_dependencies([tf.assert_equal(image.shape[:2], resized_size)]):
+        return image, img_width
 
 
 def random_rotation(img, max_rotation=0.1, crop=True):  # from SeguinBe
@@ -95,7 +122,7 @@ def augment_data(image):
         return image
 
 
-def padding_inputs_width(image, target_shape, increment=2):
+def padding_inputs_width(image, target_shape, increment):
 
     target_ratio = target_shape[1]/target_shape[0]
     # Compute ratio to keep the same ratio in new image and get the size of padding
@@ -105,6 +132,11 @@ def padding_inputs_width(image, target_shape, increment=2):
 
     new_h = target_shape[0]
     new_w = tf.cast(tf.round((ratio * new_h) / increment) * increment, tf.int32)
+    f1 = lambda: (new_w, ratio)
+    f2 = lambda: (new_h, tf.constant(1.0, dtype=tf.float64))
+    new_w, ratio = tf.case({tf.greater(new_w, 0): f1,
+                            tf.less_equal(new_w, 0): f2},
+                           default=f1, exclusive=True)
     target_w = target_shape[1]
 
     # Definitions for cases
@@ -131,7 +163,8 @@ def padding_inputs_width(image, target_shape, increment=2):
             # Count number of replications needed
             n_replication = tf.cast(tf.ceil(target_shape[1]/new_w), tf.int32)
             img_replicated = tf.tile(img_resized, tf.stack([1, n_replication, 1]))
-            pad_image = tf.image.crop_to_bounding_box(img_replicated, 0, 0, target_shape[0], target_shape[1])
+            pad_image = tf.image.crop_to_bounding_box(image=img_replicated, offset_height=0, offset_width=0,
+                                                      target_height=target_shape[0], target_width=target_shape[1])
 
             # Set manually the shape
             pad_image.set_shape([target_shape[0], target_shape[1], img_resized.get_shape()[2]])
@@ -147,44 +180,22 @@ def padding_inputs_width(image, target_shape, increment=2):
             return img_resized, target_shape
 
     # 3 cases
-    pad_image, (new_h, new_w) = tf.case({  # new_w >= target_w
-                                         tf.logical_and(tf.greater_equal(ratio, target_ratio),
-                                                        tf.greater_equal(new_w, target_w)): simple_resize,
-                                         # case 2 : new_w >= target_w/2 & new_w < target_w & ratio < target_ratio
-                                         tf.logical_and(tf.less(ratio, target_ratio),
-                                            tf.logical_and(tf.greater_equal(new_w, tf.cast(tf.divide(target_w, 2), tf.int32)),
-                                                    tf.less(new_w, target_w))): pad_fn,
-                                         # case 3 : new_w < target_w/2 & new_w < target_w & ratio < target_ratio
-                                         tf.logical_and(tf.less(ratio, target_ratio),
-                                            tf.logical_and(tf.less(new_w, target_w),
-                                                    tf.less(new_w, tf.cast(tf.divide(target_w, 2), tf.int32)))): replicate_fn
-                                         },
-                                        default=simple_resize, exclusive=True)
+    pad_image, (new_h, new_w) = tf.case(
+        {  # case 1 : new_w >= target_w
+            tf.logical_and(tf.greater_equal(ratio, target_ratio),
+                           tf.greater_equal(new_w, target_w)): simple_resize,
+            # case 2 : new_w >= target_w/2 & new_w < target_w & ratio < target_ratio
+            tf.logical_and(tf.less(ratio, target_ratio),
+                           tf.logical_and(tf.greater_equal(new_w, tf.cast(tf.divide(target_w, 2), tf.int32)),
+                                          tf.less(new_w, target_w))): pad_fn,
+            # case 3 : new_w < target_w/2 & new_w < target_w & ratio < target_ratio
+            tf.logical_and(tf.less(ratio, target_ratio),
+                           tf.logical_and(tf.less(new_w, target_w),
+                                          tf.less(new_w, tf.cast(tf.divide(target_w, 2), tf.int32)))): replicate_fn
+        },
+        default=simple_resize, exclusive=True)
 
     return pad_image, new_w  # new_w = image width used for computing sequence lengths
-
-
-def image_reading(path, resized_size=None, data_augmentation=False, padding=False):
-    # Read image
-    image_content = tf.read_file(path, name='image_reader')
-    image = tf.cond(tf.equal(tf.string_split([path], '.').values[1], tf.constant('jpg', dtype=tf.string)),
-                    true_fn=lambda: tf.image.decode_jpeg(image_content, channels=1, try_recover_truncated=True),
-                    false_fn=lambda: tf.image.decode_png(image_content, channels=1))
-
-    # Data augmentation
-    if data_augmentation:
-        image = augment_data(image)
-
-    # Padding
-    if padding:
-        with tf.name_scope('padding'):
-            image, img_width = padding_inputs_width(image, resized_size)
-    # Resize
-    elif resized_size:
-        image = tf.image.resize_images(image, size=resized_size)
-        img_width = tf.shape(image)[1]
-
-    return image, img_width
 
 
 def preprocess_image_for_prediction(fixed_height=32, min_width=8):
