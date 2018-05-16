bc2342eb17c4feca41627cdeb44c11e48d5450f3
soliveir <sofia.oliveiraares@epfl.ch>
2018-05-16 17:35:12 +0200

    updated setup with dependencies

--- setup.py
+++ setup.py
@@ -5,8 +5,18 @@ __license__ = "GPL"
 from setuptools import setup, find_packages
 
 setup(name='tf_crnn',
-      version='0.4',
+      version='0.4.1',
       url='https://github.com/solivr/tf-crnn',
       description='TensorFlow Convolutional Recurrent Neural Network (CRNN)',
+      install_requires=[
+            'tensorflow-gpu==1.4.1',
+            'imageio',
+            'tqdm',
+            'sacred',
+            'tensorflow-tensorboard==0.4.0',
+            'better_exceptions',
+            'opencv-python'
+            # missing opencv2 that needs to be installed via conda
+      ],
       packages=find_packages(where='.'),
       zip_safe=False)
