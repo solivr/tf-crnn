d6512af40db66b145c0d2f5d8bd0590af7148906
soliveir <sofia.oliveiraares@epfl.ch>
2019-01-14 12:10:37 +0100

    .

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx_autodoc_typehints'
+    # 'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
