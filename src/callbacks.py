2a9ba7b645432e0f1102416bdf33bfbcdd828e4a
soliveir <sofia.oliveiraares@epfl.ch>
2019-10-02 11:39:08 +0200

    custom prediction saver callback

--- src/callbacks.py
+++ src/callbacks.py
@@ -7,12 +7,15 @@ import os
 import pickle
 import json
 import time
+from .config import Params
+
 
 MODEL_WEIGHTS_FILENAME = 'weights.h5'
 OPTIMIZER_WEIGHTS_FILENAME = 'optimizer_weights.pkl'
 LEARNING_RATE_FILENAME = 'learning_rate.pkl'
 LAYERS_FILENAME = 'architecture.json'
 EPOCH_FILENAME = 'epoch.pkl'
+FOLDER_SAVED_MODEL = 'saving'
 
 
 class CustomSavingCallback(tf.keras.callbacks.Callback):
@@ -25,10 +28,13 @@ class CustomSavingCallback(tf.keras.callbacks.Callback):
 
         self.saving_dir = output_dir
 
-    def on_epoch_begin(self, epoch, logs=None):
+    def on_epoch_begin(self,
+                       epoch,
+                       logs=None):
         self._current_epoch = epoch
 
-    def on_train_end(self, logs=None):
+    def on_train_end(self,
+                     logs=None):
         timestamp = str(int(time.time()))
         folder = os.path.join(self.saving_dir, timestamp)
         os.makedirs(folder)
@@ -67,8 +73,10 @@ class CustomLoaderCallback(tf.keras.callbacks.Callback):
 
         self.loading_dir = loading_dir
 
+    def set_model(self, model):
+        self.model = model
 
-    def on_train_begin(self, logs=None):
+        print('-- Loading ', self.loading_dir)
         # Load model weights
         self.model.load_weights(os.path.join(self.loading_dir, MODEL_WEIGHTS_FILENAME))
 
@@ -83,24 +91,33 @@ class CustomLoaderCallback(tf.keras.callbacks.Callback):
         self.model._make_train_function()
         self.model.optimizer.set_weights(optimizer_weights)
 
-    def on_test_begin(self, logs=None):
-        # Load model weights
-        self.model.load_weights(os.path.join(self.loading_dir, MODEL_WEIGHTS_FILENAME))
 
-        # Load optimizer params
-        with open(os.path.join(self.loading_dir, OPTIMIZER_WEIGHTS_FILENAME), 'rb') as f:
-            optimizer_weights = pickle.load(f)
-        with open(os.path.join(self.loading_dir, LEARNING_RATE_FILENAME), 'rb') as f:
-            learning_rate = pickle.load(f)
+class CustomPredictionSaverCallback(tf.keras.callbacks.Callback):
+    def __init__(self,
+                 output_dir: str,
+                 parameters: Params):
+        super(CustomPredictionSaverCallback, self).__init__()
 
-        # Set optimizer params
-        self.model.optimizer.learning_rate.assign(learning_rate)
-        self.model._make_train_function()
-        self.model.optimizer.set_weights(optimizer_weights)
+        self.saving_dir = output_dir
+        self.parameters = parameters
 
-    def on_predict_begin(self, logs=None):
-        # Load model weights
+    # Inference
+    def on_predict_begin(self,
+                         logs=None):
+        # Create file to add predictions
+        timestamp = str(int(time.time()))
+        self._prediction_filename = os.path.join(self.saving_dir, 'predictions-{}.txt'.format(timestamp))
 
-        self.model.load_weights(os.path.join(self.loading_dir, MODEL_WEIGHTS_FILENAME))
+    def on_predict_batch_end(self,
+                             batch,
+                             logs):
+        logits, seq_len, filenames = logs['outputs']
 
+        codes = tf.keras.backend.ctc_decode(logits, tf.squeeze(seq_len), greedy=True)[0][0].numpy()
+        strings = [''.join([self.parameters.alphabet.lookup_int2str[c] for c in lc if c != -1]) for lc in codes]
+
+        with open(self._prediction_filename, 'ab') as f:
+            for n, s in zip(filenames, strings):
+                n = n[0] # n is a list of one element
+                f.write((n.decode() + ';' + s + '\n').encode('utf8'))
 
